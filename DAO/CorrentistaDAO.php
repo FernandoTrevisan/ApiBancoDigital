eb0d94ff8cfba89c859e154438e5efb3394a0015
fernandotrevisan <fernando.trevisan01@etec.sp.gov.br>
2023-07-31 08:46:05 -0300

    Mudanças Nas DAO's

--- DAO/CorrentistaDAO.php
+++ DAO/CorrentistaDAO.php
@@ -3,68 +3,45 @@
 namespace ApiBancoDigital\DAO;
 
 use ApiBancoDigital\Model\CorrentistaModel;
-use PDO;
 
-/**
- * As classes DAO (Data Access Object) são responsáveis por executar os
- * SQL junto ao banco de dados.
- */
+
 class CorrentistaDAO extends DAO
 {
-     /**
-     * Método construtor, sempre chamado na classe quando a classe é instanciada.
-     * Exemplo de instanciar classe (criar objeto da classe):
-     * $dao = new PessoaDAO();
-     */
+    
     public function __construct()
     {
-        /**
-         * Chamando o construtor da classe DAO, isto é, toda vez que chamos o consturo da classe DAO
-         * estamos fazendo a conexão com o banco de dados.
-         */
+       
         parent::__construct();       
     }
 
-    /**
-     * 
-     */
+    
     public function save(CorrentistaModel $m) : CorrentistaModel
     {
         return ($m->id == null) ? $this->insert($m) : $this->update($m);
     }
 
 
-    /**
-     * Método que recebe um model e extrai os dados do model para realizar o insert
-     * na tabela correspondente ao model. Note o tipo do parâmetro declarado.
-     */
+   
     private function insert(CorrentistaModel $model)
     {
-        // Trecho de código SQL com marcadores ? para substituição posterior, no prepare
+        
         $sql = "INSERT INTO correntista
                             (nome, email, cpf, data_nascimento, senha) 
                 VALUES 
                             (?, ?, ?, ?, sha1(?) ) ";
 
-        // Declaração da variável stmt que conterá a montagem da consulta. Observe que
-        // estamos acessando o método prepare dentro da propriedade que guarda a conexão
-        // com o MySQL, via operador seta "->". Isso significa que o prepare "está dentro"
-        // da propriedade $conexao e recebe nossa string sql com os devidor marcadores.
-        // Para saber mais sobre Preparated Statements, leia: https://www.codigofonte.com.br/artigos/evite-sql-injection-usando-prepared-statements-no-php
+        
         $stmt = $this->conexao->prepare($sql);
 
 
-        // Nesta etapa os bindValue são responsáveis por receber um valor e trocar em uma 
-        // determinada posição, ou seja, o valor que está em 3, será trocado pelo terceiro ?
-        // No que o bindValue está recebendo o model que veio via parâmetro e acessamos
-        // via seta qual dado do model queremos pegar para a posição em questão.
+        
         $stmt->bindValue(1, $model->nome);
         $stmt->bindValue(2, $model->email);
         $stmt->bindValue(3, $model->cpf);
         $stmt->bindValue(4, $model->data_nascimento);
         $stmt->bindValue(5, $model->senha);
 
-         // Ao fim, onde todo SQL está montando, executamos a consulta.
+         
         $stmt->execute();
 
         $model->id = $this->conexao->lastInsertId();
@@ -73,18 +50,13 @@ class CorrentistaDAO extends DAO
     }
 
 
-    /**
-     * 
-     */
+    
     private function update(CorrentistaModel $m) {
 
     }
 
 
-    /**
-     * Retorna um registro específico da tabela pessoa do banco de dados.
-     * Note que o método exige um parâmetro $id do tipo inteiro.
-     */
+    
     public function selectByCpfAndSenha($cpf, $senha) : CorrentistaModel
     {
         $sql = "SELECT * FROM correntista WHERE cpf = ? AND senha = sha1(?) ";
@@ -94,20 +66,10 @@ class CorrentistaDAO extends DAO
         $stmt->bindValue(2, $senha);
         $stmt->execute();
 
-        return $stmt->fetchObject("ApiBancoDigital\Model\CorrentistaModel"); // Retornando um objeto específico PessoaModel
+        
+        $obj = $stmt->fetchObject("App\Model\CorrentistaModel");
+
+        
+        return is_object($obj) ? $obj : new CorrentistaModel();
     }
-
-
-    /**
-     * Remove um registro da tabela pessoa do banco de dados.
-     * Note que o método exige um parâmetro $id do tipo inteiro.
-     */
-    /*public function delete(int $id)
-    {
-        $sql = "UPDATE Reclamacao SET ativo='N' WHERE id = ? ";
-
-        $stmt = $this->conexao->prepare($sql);
-        $stmt->bindValue(1, $id);
-        $stmt->execute();
-    }*/
 }
